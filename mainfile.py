e0b2c2cc80c0fd4c913e0cdf816caaed307e3e25
nekituall <nekituall@gmail.com>
2022-12-23 13:36:57 +0300

    add_contact function refactored

--- mainfile.py
+++ mainfile.py
@@ -1,11 +1,68 @@
 
 
-def menu():
+def menu(login):
     """main menu func"""
-    print("Here is a list of available commands: \n 1. List all \n 2. Sort by \n 3. Filter by \n 4. Search by name \n "
-          "5. Exit")
+    print(f"welcome {login}")
+    var = int(input("Here are available commands: \n 1. Modify contacts \n 2. List my contacts \n 3. Sort by \n"
+                   " 4. Filter by \n 5. Search by name \n 6. Exit \n\n Enter command number:  "))
+    while True:
+        if var == 1:
+            modify()
+            break
+        elif var == 2:
+            list_all()
+            break
+        elif var == 3:
+            sort_by()
+            break
+        elif var == 4:
+            filter_by()
+            break
+        elif var == 5:
+            search_by_name()
+            break
+        elif var == 6:
+            break
+        else:
+            print("Command not found!")
+            case()
 
     print("end of menu")
+
+
+# def menu(user):
+#     """main menu func"""
+#     print("Here is a list of available commands: \n 1. List all \n 2. Sort by \n 3. Filter by \n 4. Search by name \n "
+#           "5. Exit")
+#
+#     print("end of menu")
+
+
+def modify():
+    var = int(input("Here are available commands: \n 1. Add  \n 2. Modify \n 3. Delete "))
+    while True:
+        if var == 1:
+            add_contact(user)
+            break
+        elif var == 2:
+            modify_contact(user)
+            break
+        elif var == 3:
+            del_contact(user)
+            break
+        else:
+            print("No such command! Re-enter")
+
+
+
+
+
+def case():
+    case = input("Exit program? : yes/no   ")
+    if case == "yes" or case == "y":
+        exit()
+    else:
+        menu()
 
 
 
@@ -15,5 +72,9 @@ def menu():
 
 
 if __name__ == '__main__':
-    print('Welcome to contact_app! \n')
-    menu()
+    login = input('Welcome to contact_app! \nPlease sign in below \nLogin:  ')
+    passw = input('Password:  ')
+    if login == "admin" and passw == "admin":
+        admin_menu(login)
+    else:
+        menu(login)
--- model_SQLlite.py
+++ model_SQLlite.py
@@ -1,20 +1,59 @@
 import sqlite3
 from sqlite3 import Error
+login = "user"
 
-def create_connection(path):
-    connection = None
+def connect_db():
     try:
-        connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
+        con = sqlite3.connect("connect_app.db")
+        print("Connection to SQLite DB established successfully!")
     except Error as e:
         print(f"The error '{e}' occurred")
 
-    return connection
+    cur = con.cursor()   #создаем обьект курсор для запросов
 
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS contacts (
+      surname TEXT NOT NULL,
+      name TEXT NOT NULL, 
+      secname TEXT NOT NULL,
+      company TEXT NOT NULL,
+      job TEXT NOT NULL,
+      email TEXT NOT NULL,
+      phone TEXT NOT NULL,
+      active TEXT NOT NULL,
+      user TEXT NOT NULL
+    )
+    """)
 
-def add_contact():
-    pass
+    con.commit()
+    return cur
 
+def add_contact(cur,login):
+    "function to add contact into DB, no checks for input data"
+    try:
+        while True:
+            surname = input("Type in Surname:  ")
+            name = input("Type in Name:  ")
+            secname = input("Type in Second name:  ")
+            company = input("Type in company name:  ")
+            job = input("Type in job:  ")
+            email = input("Type in email:  ")
+            tel = input("Type in phone number:  ")
+            cont = (surname, name, secname, company, job, email, tel, "valid", login)
+            choice = input(f"{cont} \nis this info correct? yes/no:     ")
+            if choice == "yes" or choice == "y":
+                cur.execute("INSERT INTO contacts VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);", cont)
+                print("Contact has been added")
+                add = input("Add more contact? yes/no:      ")
+                if add != "yes" or "y":
+                    break
+            else:
+                print('Okay, Let`s try again')
+    except Exception as e:
+        print(f"{e} occured")
+
+cur = connect_db()
+add_contact(cur,login)
 
 def edit_contact():
     pass
