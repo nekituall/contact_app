ba83aaf2f32db928da3618522a04b22152612f6f
nekituall <nekituall@gmail.com>
2022-12-23 09:40:59 +0300

    main menu file

---
+++ mainfile.py
@@ -0,0 +1,19 @@
+
+
+def menu():
+    """main menu func"""
+    print("Here is a list of available commands: \n 1. List all \n 2. Sort by \n 3. Filter by \n 4. Search by name \n "
+          "5. Exit")
+
+    print("end of menu")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('Welcome to contact_app! \n')
+    menu()
